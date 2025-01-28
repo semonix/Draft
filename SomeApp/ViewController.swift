3989d037f566167777df9f656b0a56e1772a42ed
semonix <semonix@github.com>
2025-01-28 21:04:13 +0300

    Add library keychain-swift

--- SomeApp/ViewController.swift
+++ SomeApp/ViewController.swift
@@ -4,8 +4,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .black
+        
+//        view.backgroundColor = .black
     }
 
 
