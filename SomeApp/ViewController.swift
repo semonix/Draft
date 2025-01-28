ec37e7844526b66cfd4f32347db9111a93b022cd
semonix <prod_uplinks_00@icloud.com>
2025-01-28 21:30:43 +0300

    Homework "Сохранение счётчика в UserDefaults" is done

--- SomeApp/ViewController.swift
+++ SomeApp/ViewController.swift
@@ -4,19 +4,34 @@ import SnapKit
 class ViewController: UIViewController {
     
     let label = UILabel()
+    let key = "counterKey"
+    let uds = UserDefaults.standard
+    var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(label)
 //        view.backgroundColor = .black
         label.snp.makeConstraints { make in
-            make.height.equalTo(50)
+            make.height.equalTo(100)
             make.width.equalTo(200)
             make.center.equalToSuperview()
         }
-        label.textAlignment = .center
-//        label.text = "dshbsfgb"
         
+        label.textAlignment = .center
+        counter = uds.integer(forKey: key)
+//        if counter == 0 {
+//            counter = 1
+//        } else {
+//            counter += 1
+//        }
+        counter += 1
+        uds.set(counter, forKey: key)
+
+        
+        label.backgroundColor = .lightGray
+        label.font = .systemFont(ofSize: 50)
+        label.text = String(counter)
     }
     
     
