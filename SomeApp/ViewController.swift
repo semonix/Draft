1bca0e7742c8abc7fc60532649df0346f9455018
semonix <prod_uplinks_00@icloud.com>
2025-01-28 21:13:23 +0300

    Add label

--- SomeApp/ViewController.swift
+++ SomeApp/ViewController.swift
@@ -1,14 +1,25 @@
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.addSubview(label)
 //        view.backgroundColor = .black
+        label.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.width.equalTo(200)
+            make.center.equalToSuperview()
+        }
+        label.textAlignment = .center
+//        label.text = "dshbsfgb"
+        
     }
-
-
+    
+    
 }
 
 @available(iOS 17.0, *)
