bcc664523332a4554d44bd5cabe8ade890a6e32d
semonix <prod_uplinks_00@icloud.com>
2025-02-03 17:30:40 +0300

    Lession "Multithreading в swift с нуля-  урок 9  - GCD  Async After, Concurrent Perform, Initially Inactive" is done

--- SomeApp/VC/MyViewController.swift
+++ SomeApp/VC/MyViewController.swift
@@ -1,38 +1,67 @@
 import UIKit
 import SnapKit
-
+protocol MyP {
+    var computedTypeProperty: Int { get }
+}
 class MyViewController: UIViewController {
     let button = UIButton()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(button)
-        title = "vc 1"
         
-        view.backgroundColor = .systemCyan.withAlphaComponent(0.5)
-        button.addTarget(self, action: #selector(pressAction), for: .touchUpInside)
+        addSubviews()
+        setupLayout()
+        setupAppearance()
+        setupTargets()
+
+//        afterBlock(seconds: 2, queue: .global(qos: .utility)) {
+//            print("Hello")
+//        }
+        
+//        afterBlock(seconds: 2) {
+//            self.showAllert()
+//        }
     }
-    @objc func pressAction() {
+}
+// MARK: - EXAMPLE OF GCD
+extension MyViewController {
+    func afterBlock(seconds: Int, queue: DispatchQueue = DispatchQueue.global(), completion: @escaping () -> Void) {
+        queue.asyncAfter(deadline: .now() + .seconds(seconds), execute: completion)
+    }
+    func showAllert() {
+        let alert = UIAlertController(title: nil, message: "message", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(action)
+        
+        DispatchQueue.main.async {
+            self.present(alert, animated: true)
+        }
+    }
+}
+// MARK: - SETUP
+extension MyViewController {
+    func addSubviews() {
+        view.addSubview(button)
+    }
+    func setupLayout() {
+        button.snp.makeConstraints { make in
+            make.height.equalTo(60)
+            make.width.equalTo(200)
+            make.center.equalToSuperview()
+        }
+    }
+    func setupAppearance() {
+        view.backgroundColor = .systemCyan.withAlphaComponent(0.5)
+        button.backgroundColor = .systemRed.withAlphaComponent(0.9)
+        button.setTitle("Press", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 37)
+    }
+    func setupTargets() {
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+    }
+    @objc func buttonPressed() {
         let secondVC = SecondViewController()
         navigationController?.pushViewController(secondVC, animated: true)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        initButton()
-    }
-    func initButton() {
-        button.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            button.heightAnchor.constraint(equalToConstant: 50),
-            button.widthAnchor.constraint(equalToConstant: 200),
-            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-        ])
-        button.setTitle("Press", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemRed//.green
-        button.layer.cornerRadius = 10
     }
 }
 
