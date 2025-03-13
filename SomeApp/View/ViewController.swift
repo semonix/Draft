2247feae7aa2dd4e0dcc9bfcafc0ffe3802b036f
semonix <prod_uplinks_00@icloud.com>
2025-03-13 18:55:57 +0300

    Install and configure the coordinator

---
+++ SomeApp/View/ViewController.swift
@@ -0,0 +1,41 @@
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController, Coordinating {
+    
+    var coordinator: Coordinator?
+    let button = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        title = "Home"
+        
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.width.equalTo(220)
+            make.height.equalTo(55)
+            make.center.equalToSuperview()
+        }
+        button.backgroundColor = .systemGreen
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        button.setTitle("Tap Me!", for: .normal)
+    }
+    @objc func didTapButton() {
+        coordinator?.eventOccured(with: .buttonTapped)
+    }
+    deinit {
+        print("ViewController покинул здание")
+    }
+}
+
+//@available(iOS 17.0, *)
+//#Preview {
+//    UINavigationController(rootViewController: ViewController())
+    
+//    let vc = ViewController()
+//    let navC = vc.coordinator?.navigationController
+//    
+//    UINavigationController(rootViewController: navC!)
+//}
