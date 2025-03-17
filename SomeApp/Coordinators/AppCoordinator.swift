7ff832cc04bfd09db2a0770baafd4174f72eb4f0
semonix <prod_uplinks_00@icloud.com>
2025-03-17 15:46:40 +0300

    Lession "Coordinator pattern. Swift. iOS" is done

--- SomeApp/Coordinators/AppCoordinator.swift
+++ SomeApp/Coordinators/AppCoordinator.swift
@@ -2,14 +2,32 @@ import UIKit
 
 class AppCoordinator: Coordinator {
     var navigationController: UINavigationController
-    
     var flowCompletionHandler: CoordinatorHandler?
     
-    func start() {
-        
-    }
+    private var childCoordinators: [Coordinator] = []
+    
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
-    
+    func start() {
+        let isAuth = false
+        
+        if isAuth {
+            showMainFlow()
+        } else {
+            showRegistrationFlow()
+        }
+    }
+    private func showRegistrationFlow() {
+        let registrationCoordinator = CoordinatorFactory().createRegistrationCoordinator(navigationController: navigationController)
+        childCoordinators.append(registrationCoordinator)
+        registrationCoordinator.flowCompletionHandler = { [weak self] in
+            self?.showMainFlow()
+        }
+        
+        registrationCoordinator.start()
+    }
+    private func showMainFlow() {
+        navigationController.setViewControllers([MainViewController()], animated: true)
+    }
 }
